38fb1fa77cba05429fc3d7c5cab3ca646d6e0f8b
Ivan <76810537+luntikius@users.noreply.github.com>
2026-06-18 13:45:22 +0300

    Remove PKPass trailing comma unit test

--- app/src/main/java/com/luntikius/wallet/data/json/WalletJson.kt
+++ app/src/main/java/com/luntikius/wallet/data/json/WalletJson.kt
@@ -1,10 +1,13 @@
 package com.luntikius.wallet.data.json
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalSerializationApi::class)
 object WalletJson {
     val json = Json {
         ignoreUnknownKeys = true
         encodeDefaults = true
+        allowTrailingComma = true
     }
 }
